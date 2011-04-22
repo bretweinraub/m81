a6a6a9408ec90d0f5bf9c7179988b0b1d37bb44c
bretweinraub <bret@ckuru.com>
2011-04-22 17:52:27 +0000

    add in and take out

--- sm/src/ChainDB/src/code/genlinks.sql
+++
@@ -1,2 +0,0 @@
-	'<a href="http://' || 'usbohp380-7/' || substr (collections.value, instr (collections.value, '/', 1, 3) + 1, instr (collections.value, '/', 1, 4) - instr (collections.value, '/', 1, 3) - 1) || '/wlogs/' || task.task_id || '/">link</a>' results,
-	'<a href="http://' || 'usbohp380-7/' || substr (collections.value, instr (collections.value, '/', 1, 3) + 1, instr (collections.value, '/', 1, 4) - instr (collections.value, '/', 1, 3) - 1) || '/taskData/' || task.task_id || '/">link</a>' internal,
